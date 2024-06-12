6ac7164985aab3f68c613ae19ca9d6d21142e250
Maggie Boyse <maggie_boyse@outlook.com>
2024-06-12 15:12:44 -0400

    0.5 form post api call function

--- src/components/movieForm/movieForm.js
+++ src/components/movieForm/movieForm.js
@@ -1,24 +1,85 @@
 import "../movieForm/movieForm.scss";
-// import { API_URL } from "../../utils/api";
-// import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+import { API_URL } from "../../utils/api";
 
-const Form = () => {
+function MovieForm() {
+  const [movieTitle, setMovieTitle] = useState("");
+  const handleMovieTitleChange = (e) => {
+    setMovieTitle(e.target.value);
+  };
+
+  const [movieYear, setMovieYear] = useState("");
+  const handleMovieYearChange = (e) => {
+    setMovieYear(e.target.value);
+  };
+
+  const [movieDuration, setMovieDuration] = useState("");
+  const handleMovieDurationChange = (e) => {
+    setMovieDuration(e.target.value);
+  };
+
+  const [movieLink, setMovieLink] = useState("");
+  const handleMovieLinkChange = (e) => {
+    setMovieLink(e.target.value);
+  };
+
+  const [movieDescription, setMovieDescription] = useState("");
+  const handleMovieDescriptionChange = (e) => {
+    setMovieDescription(e.target.value);
+  };
+
+  const handleSubmitForm = async (e) => {
+    e.preventDefault();
+    try {
+      const newMovie = {
+        title: movieTitle,
+        year: movieYear,
+        duration: movieDuration,
+        link: movieLink,
+        description: movieDescription,
+      };
+      await axios.post(`${API_URL}/movies`, newMovie);
+    } catch (error) {
+      console.log(error, "cannot post right now");
+    }
+  };
+
   return (
-    <section className="form">
+    <form className="form" onSubmit={handleSubmitForm}>
       <h2 className="form__main-title">Add a new movie!</h2>
       <div className="form__input-fields">
-        <input className="form__title" placeholder="title"></input>
-        <input className="form__year" placeholder="year"></input>
-        <input className="form__duration" placeholder="duration"></input>
+        <input
+          className="form__title"
+          placeholder="title"
+          onChange={handleMovieTitleChange}
+        ></input>
+        <input
+          className="form__year"
+          placeholder="year"
+          onChange={handleMovieYearChange}
+        ></input>
+        <input
+          className="form__duration"
+          placeholder="duration"
+          onChange={handleMovieDurationChange}
+        ></input>
+        <input
+          className="form__link"
+          placeholder="link"
+          onChange={handleMovieLinkChange}
+        ></input>
         <textarea
           className="form__description"
           placeholder="description"
+          onChange={handleMovieDescriptionChange}
         ></textarea>
-        <input className="form__link" placeholder="link"></input>
-        <button className="form__button">ADD MOVIE</button>
+        <button className="form__button" type="submit">
+          ADD MOVIE
+        </button>
       </div>
-    </section>
+    </form>
   );
-};
+}
 
-export default Form;
+export default MovieForm;
--- src/pages/homePage/homePage.js
+++ src/pages/homePage/homePage.js
@@ -1,12 +1,12 @@
 import "../homePage/homePage.scss";
 import Header from "../../components/header/header";
-import Form from "../../components/movieForm/movieForm";
+import MovieForm from "../../components/movieForm/movieForm";
 
 function HomePage() {
   return (
     <div className="home-page">
       <Header />
-      <Form />
+      <MovieForm />
     </div>
   );
 }

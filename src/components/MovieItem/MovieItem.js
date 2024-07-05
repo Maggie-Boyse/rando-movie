572b1ec5a9b5501906796f7de8ef6575a22d9eda
Maggie Boyse <maggie_boyse@outlook.com>
2024-07-05 12:55:29 -0400

    0.11 initialize delete movie

--- src/components/MovieItem/MovieItem.js
+++ src/components/MovieItem/MovieItem.js
@@ -1,6 +1,22 @@
 import "../MovieItem/MovieItem.scss";
+import { useParams } from "react-router-dom";
+import { API_URL } from "../../utils/api";
+// import { useEffect } from "react";
+import axios from "axios";
 
 function MovieItem({ movie }) {
+  const { movieId } = useParams;
+  const deleteMovie = async (movieId) => {
+    try {
+      await axios.delete(`${API_URL}/horror/${movieId}`);
+      console.log("Item deleted:", movieId);
+    } catch (error) {
+      console.error("Error deleting item:", error);
+    }
+  };
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
   return (
     <section className="movie-item" key={movie.id}>
       <h3 className="movie-item__title">{movie.title}</h3>
@@ -8,6 +24,12 @@ function MovieItem({ movie }) {
         <p className="movie-item__year">{movie.year}</p>
         <p className="movie-item__duration">{movie.duration}</p>
       </div>
+      <button
+        className="movie-item__button"
+        onClick={() => deleteMovie(movieId)}
+      >
+        delete
+      </button>
     </section>
   );
 }
--- src/pages/homePage/homePage.js
+++ src/pages/homePage/homePage.js
@@ -2,14 +2,14 @@ import "../homePage/homePage.scss";
 import Header from "../../components/Header/Header";
 import MovieForm from "../../components/MovieForm/MovieForm";
 import MovieList from "../../components/MovieList/MovieList";
-import RandomReturn from "../../components/RandomReturn/RandomReturn";
+// import RandomReturn from "../../components/RandomReturn/RandomReturn";
 
 function HomePage() {
   return (
     <div className="home-page">
       <Header />
       <MovieForm />
-      <RandomReturn/>
+      {/* <RandomReturn/> */}
       <MovieList />
     </div>
   );
